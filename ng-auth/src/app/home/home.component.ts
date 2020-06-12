c8465d78e593454306f74850b9364307212a6bd7
Muhammed Saleem P <muhammedsalimp@gmail.com>
2020-06-12 16:54:27 +0530

    Add files via upload

---
+++ ng-auth/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: []
+})
+export class HomeComponent {
+
+  constructor(private jwtHelper: JwtHelperService, private router: Router) {
+  }
+
+  isUserAuthenticated() {
+    let token: string = localStorage.getItem("jwt");
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  public logOut = () => {
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("refreshToken");
+  }
+}
---
+++ ng-auth/src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  invalidLogin: boolean;
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+  public login = (form: NgForm) => {
+    let credentials = JSON.stringify(form.value);
+    this.http.post("http://localhost:5000/api/auth/login", 
+    credentials, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })          
+    }).subscribe(response => {      
+      let token = (<any>response).token;
+      let refreshToken = (<any>response).refreshToken;
+      localStorage.setItem("jwt", token);
+      localStorage.setItem("refreshToken", refreshToken);
+      this.invalidLogin = false;
+      this.router.navigate(["/"]);
+    }, err => {
+      this.invalidLogin = true;
+    });
+  }
+}
