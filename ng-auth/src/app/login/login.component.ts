97c7b1c76ddeb1c7de4794d804911d335cdb533a
Muhammed Saleem P <muhammedsalimp@gmail.com>
2020-06-13 00:32:09 +0530

    Update login.component.ts

--- ng-auth/src/app/login/login.component.ts
+++ ng-auth/src/app/login/login.component.ts
@@ -1,53 +1,33 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { NgForm } from '@angular/forms';
 
-@Injectable()
-export class AuthGuard implements CanActivate {
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  invalidLogin: boolean;
 
-  constructor(private jwtHelper: JwtHelperService, private router: Router, private http: HttpClient) {
+  constructor(private router: Router, private http: HttpClient) { }
+
+  public login = (form: NgForm) => {
+    const credentials = JSON.stringify(form.value);
+    this.http.post("http://localhost:5000/api/auth/login", 
+    credentials, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })          
+    }).subscribe(response => {      
+      const token = (<any>response).token;
+      const refreshToken = (<any>response).refreshToken;
+      localStorage.setItem("jwt", token);
+      localStorage.setItem("refreshToken", refreshToken);
+      this.invalidLogin = false;
+      this.router.navigate(["/"]);
+    }, err => {
+      this.invalidLogin = true;
+    });
   }
-  async canActivate() {
-    const token = localStorage.getItem("jwt");
-
-    if (token && !this.jwtHelper.isTokenExpired(token)) {
-      console.log(this.jwtHelper.decodeToken(token));
-      return true;
-    }
-
-    const isRefreshSuccess = await this.tryRefreshingTokens(token);
-    if (!isRefreshSuccess) {
-      this.router.navigate(["login"]);
-    }
-
-    return isRefreshSuccess;
-  }
-
-  private async tryRefreshingTokens(token: string): Promise<boolean> {
-    // Try refreshing tokens using refresh token
-    let refreshToken: string = localStorage.getItem("refreshToken");
-    const credentials = JSON.stringify({ accessToken: token, refreshToken: refreshToken });
-
-    let isRefreshSuccess: boolean;
-    try {
-      const response = await this.http.post("http://localhost:5000/api/token/refresh", credentials, {
-        headers: new HttpHeaders({
-          "Content-Type": "application/json"
-        }),
-        observe: 'response'
-      }).toPromise();
-      // If token refresh is successful, set new tokens in local storage.
-      const newToken = (<any>response).body.accessToken;
-      const newRefreshToken = (<any>response).body.refreshToken;
-      localStorage.setItem("jwt", newToken);
-      localStorage.setItem("refreshToken", newRefreshToken);
-      isRefreshSuccess = true;
-    }
-    catch (ex) {      
-      isRefreshSuccess = false;
-    }
-    return isRefreshSuccess;
-  }
-
 }
